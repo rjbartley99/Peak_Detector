79da66c2ae23a3f8c075f2a7e29d08a88227db7a
WillMyers1 <61232716+WillMyers1@users.noreply.github.com>
2020-05-06 05:04:05 +0100

    Add files via upload

---
+++ FormatTable.vhd
@@ -0,0 +1,80 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06.05.2020 00:09:09
+-- Design Name: 
+-- Module Name: FormatTable - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+entity formatmux is
+    Port ( 
+        clk : in std_logic;
+        countIn : in std_logic_vector(5 downto 0);
+        formatout: out std_logic_vector(7 downto 0)
+        );
+end formatmux;
+
+architecture Behavioral of formatmux is
+signal q1 : std_logic_vector(7 downto 0); 
+begin
+
+format_look : process (countIn)
+begin
+    CASE countIn is
+       
+         
+          WHEN "000100" =>
+       q1 <= "01011100";--\
+           
+          WHEN "000101" => 
+       q1 <= "01101110";--n
+         
+          WHEN "000110" =>
+       q1 <= "01011100";--\
+           
+          WHEN "000111" => 
+       q1 <= "01110010";--r
+          
+          WHEN "001000" => 
+       q1 <= "00111101";--=    
+          
+          WHEN "001001" => 
+       q1 <= "00111101";--= 
+          
+          WHEN others =>
+         q1 <= "00100000";
+       end case;
+end process format_look;
+
+    PROCESS(clk)
+    begin
+     if rising_edge(clk)then 
+       formatout <= q1;
+     else 
+       null;
+     end if;
+   end PROCESS;
+
+end Behavioral;
+
---
+++ SeqDoneCounter.vhd
@@ -0,0 +1,63 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05.05.2020 20:02:09
+-- Design Name: 
+-- Module Name: SeqDoneCounter - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity seqdonecounter is
+port(
+    clk : in std_logic;
+    rst : in std_logic;
+    en : in std_logic;
+    SEQcntOut : out std_logic_vector(1 downto 0)
+    );
+end seqdonecounter;
+
+architecture Behavioral of seqdonecounter is
+    signal counter, counter_next : std_logic_vector(1 downto 0);
+begin
+    process(clk)
+    begin
+           if rst = '1' then
+              counter <= "00";
+           elsif clk'EVENT AND clk='1' then
+              counter <= counter_next;
+           end if;
+    end process;
+
+    counter_comb : process(counter, en)
+    begin
+        if en = '0' then
+              counter_next <= counter;
+        elsif counter = "11" then
+              counter_next<= counter;
+        else
+              counter_next <= std_logic_vector(unsigned(counter) + 1);
+        end if;
+    end process;
+
+    cntOut_comb : process(counter)
+    begin
+        SEQcntOut <= counter;
+    end process;
+end Behavioral;
+
