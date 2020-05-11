0beb592881ec4ce7e0f077b64231edb69e800ea7
WillMyers1 <will.myers@btconnect.com>
2020-05-11 16:55:59 +0100

    Update myRegister.vhd

--- myRegister.vhd
+++ myRegister.vhd
@@ -5,20 +5,22 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity myRegister is 
 port(
-  clk: in std_ulogic;
-  regreset: in std_ulogic;
-  load: in std_ulogic;
-  D: in std_ulogic_vector(7 downto 0);
-  Q: out std_ulogic_vector(7 downto 0)
+  clk: in std_logic;
+  regreset: in std_logic;
+  load: in std_logic;
+  D: in std_logic_vector(7 downto 0);
+  Q: out std_logic_vector(7 downto 0)
   );
 end myRegister; 
 
 ARCHITECTURE Behavioral of myRegister is
 
 begin
-    PROCESS(clk, load)
+    PROCESS(clk,regreset)
     begin
-     if rising_edge(clk) and load = '1' then 
+     if regreset ='1' then
+     Q <="11111111";
+     elsif rising_edge(clk) and load = '1' then 
        Q <= D;
      else 
        null;
